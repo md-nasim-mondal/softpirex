4fe9d7fdd935d49d5a2f129b7b5b33eb60648a83
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-02-22 12:18:29 +0600

    sell your product section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Banner from "@/components/HomePage/Banner";
 import HeroSection from "@/components/HomePage/Demos";
 import FeaturesSection from "@/components/HomePage/Features";
+import SellYourProducts from "@/components/HomePage/SellYourProducts";
 
 export default function Home() {
   return (
     <div className="">
-<Banner />
-<FeaturesSection />
-<HeroSection />
+      <Banner />
+      <FeaturesSection />
+      <HeroSection />
+      <SellYourProducts/>
     </div>
   );
 }
