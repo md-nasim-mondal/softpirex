fbdac4c78a4a5762eeca504ceb29a410c7457282
Md. Nasim Mondal <nasim.programmer24@gmail.com>
2025-03-02 00:50:29 +0600

    added modal ui components file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -84,12 +84,12 @@ const Login = () => {
         </div>
         <form
           onSubmit={handleSubmit(handleLogin)}
-          className='w-full flex flex-col gap-6 px-4 lg:px-8 py-4 lg:py-6 shadow-lg shadow-blue-500 border border-white rounded-md'>
+          className='w-full flex flex-col px-4 lg:px-8 py-4 lg:py-6 shadow-lg shadow-blue-500 border border-white rounded-md'>
           <h2 className='text-xl md:text-2xl font-medium font-kreonSerif'>
             Login with Email & Password
           </h2>
           {/* Email */}
-          <div className='flex flex-col gap-3'>
+          <div className='flex flex-col gap-3 my-6'>
             <div className='flex items-center gap-2 pl-2 bg-transparent rounded-lg border border-white'>
               <label htmlFor='email'>
                 <MdEmail />
@@ -139,9 +139,12 @@ const Login = () => {
               <p className='text-red-700'>{errors.password.message}</p>
             )}
           </div>
+          <div className="text-sm font-medium text-blue-600 text-end mt-2 hover:underline hover:text-white">
+            forgot password?
+          </div>
           <button
             type='submit'
-            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-lg px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-lg px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-6'
             disabled={userLoading}>
             {userLoading ? buttonLoader : "Login!"}
           </button>
