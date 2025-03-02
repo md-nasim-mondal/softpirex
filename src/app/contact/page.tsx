0dfc43e6d54c87689c9d103b940e85b23f6cd7e9
Md. Nasim Mondal <nasim.programmer24@gmail.com>
2025-03-02 23:08:31 +0600

    try to contact page problem solve with ignore case

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Contacts from "@/components/Contacts/Contacts";
+
+const ContactPage = () => {
+  return (
+    <main className='bg-gray-900 min-h-screen flex justify-center items-center'>
+      <Contacts />
+      {/* This is contact page */}
+    </main>
+  );
+};
+
+export default ContactPage;
