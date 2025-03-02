6a95c018fecba2586a5290a02e0c9ebe342f8e9f
Md. Nasim Mondal <nasim.programmer24@gmail.com>
2025-03-02 23:10:32 +0600

    Fixed case sensitivity issue

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -4,6 +4,7 @@ const ContactPage = () => {
   return (
     <main className='bg-gray-900 min-h-screen flex justify-center items-center'>
       <Contacts />
+      {/* This is contact page */}
     </main>
   );
 };
